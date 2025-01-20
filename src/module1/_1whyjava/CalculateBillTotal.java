db6c716b5fb9fcba75b74534094e62ca4a45d0c5
alexanderchen <alexanderchen@localhost>
2025-01-19 18:43:16 -0600

    Done.

--- src/module1/_1whyjava/CalculateBillTotal.java
+++ src/module1/_1whyjava/CalculateBillTotal.java
@@ -4,8 +4,8 @@ public class CalculateBillTotal
 {
    public static void main(String[] args)
    {
-      System.out.println("Bill total: 35");
-
+      System.out.println("Bill total: 89.50 + 14.75 + 45.12 + 92.50 = ");
+      System.out.println(89.50 + 14.75 + 45.12 + 92.50);
    }
 }
 
