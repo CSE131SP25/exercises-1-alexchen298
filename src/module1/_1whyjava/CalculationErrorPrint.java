99ff9fb479b207e4fe8681178063ae1fc0409943
alexanderchen <alexanderchen@localhost>
2025-01-19 18:30:13 -0600

    Done.

--- src/module1/_1whyjava/CalculationErrorPrint.java
+++ src/module1/_1whyjava/CalculationErrorPrint.java
@@ -5,6 +5,6 @@ public class CalculationErrorPrint
    public static void main(String[] args)
    {
       System.out.println("570 * 23 = ");
-      System.out.println("570 * 23");
+      System.out.println(570 * 23);
    }
 }
