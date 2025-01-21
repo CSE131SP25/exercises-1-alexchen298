7b223134816f06eabea693933bce3b96c9f75799
alexanderchen <alexanderchen@localhost>
2025-01-21 11:31:49 -0600

    Done.

--- src/module1/_7math/Test4.java
+++ src/module1/_7math/Test4.java
@@ -4,6 +4,6 @@ public class Test4
 {
    public static void main(String[] args)
    {
-     System.out.println((int) (Math.random() * 10));
+     System.out.println((int) (Math.random() * 99));
    }
 }
