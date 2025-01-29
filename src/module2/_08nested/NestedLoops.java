f576adef4a0855af7531dba9374462d1c420ce81
alexanderchen <alexanderchen@localhost>
2025-01-29 01:58:05 -0600

    Finished Module 2.08

--- src/module2/_08nested/NestedLoops.java
+++ src/module2/_08nested/NestedLoops.java
@@ -5,9 +5,9 @@ public class NestedLoops
 
    public static void main(String[] args)
    {
-       for (int row = 1; row <= 3; row++)
+       for (int row = 1; row <= 10; row++)
        {
-           for (int col = 1; col <= 5; col++)
+           for (int col = 1; col <= 8; col++)
            {
                System.out.print("*");
            }
